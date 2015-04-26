d79d6e14953121b2a055862812f1295bbb5c2f5d
Meinolf Droste <mdwp.post@gmail.com>
2015-04-26 21:13:57 +0200

    bugfix

--- config.rb
+++ config.rb
@@ -70,7 +70,8 @@ configure :build do
 
   # For example, change the Compass output style for deployment
   activate :minify_css
-  
+  #activate :minify_html, remove_input_attributes: false
+  activate :inliner
   activate :gzip
 
   # Minify Javascript on build
