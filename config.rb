e839dc39eb4fcbc20c8d3b6108faa7f5c6d5eb20
Meinolf Droste <m.droste@mdwp.de>
2015-04-29 17:42:32 +0200

    inline css and js

--- config.rb
+++ config.rb
@@ -69,13 +69,15 @@ end
 configure :build do
 
   # For example, change the Compass output style for deployment
-  activate :minify_css
-  #activate :minify_html, remove_input_attributes: false
+  activate :minify_css,
+    :inline=>true
+  activate :minify_html
   #activate :inliner
-  #activate :gzip
+  activate :gzip
 
   # Minify Javascript on build
-  activate :minify_javascript
+  activate :minify_javascript,
+    :inline=>true
 
   # Enable cache buster
   activate :cache_buster
